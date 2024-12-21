90372961aac02d71b495040eb20938b550ea157e
xie392 <ymzdjiang@qq.com>
2024-12-21 10:53:18 +0800

    ci: update README.md and hone page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Link from 'next/link'
 
 export default function RootPage() {
     return (
-        <h1>Hello World</h1>
+        <div>
+            <h1 className="text-3xl font-bold mb-4">更新中...</h1>
+            <Link href="/docs/介绍" className="text-blue-500 hover:underline">
+                预览文档
+            </Link>
+        </div>
     )
 }
